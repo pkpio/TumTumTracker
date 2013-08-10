e1b327723af58c295437ef74ab39bce22173fec5
Praveen Kumar Pendyala <praveendath92@gmail.com>
2013-08-10 06:49:52 +0530

    Version 3.2 Release preview
    -Fixed all FCs due to NULL pointers
    -Added detailed help section
    -In app change log and version display
    -Lot more that I don't remember :P

--- gen/in/co/praveenkumar/tumtumtracker/R.java
+++ gen/in/co/praveenkumar/tumtumtracker/R.java
@@ -15,24 +15,29 @@ public final class R {
         public static final int black=0x7f040000;
     }
     public static final class drawable {
-        public static final int androidmarker=0x7f020000;
+        public static final int androidmarker_black=0x7f020000;
         public static final int androidmarker_blue=0x7f020001;
         public static final int androidmarker_green=0x7f020002;
         public static final int androidmarker_red=0x7f020003;
         public static final int androidmarker_yellow=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int ic_launcher_big=0x7f020006;
-        public static final int jithu=0x7f020007;
-        public static final int praveen=0x7f020008;
-        public static final int sampath=0x7f020009;
-        public static final int sudheer=0x7f02000a;
+        public static final int bus_stop=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int ic_launcher_big=0x7f020007;
+        public static final int jithu=0x7f020008;
+        public static final int last_updated_marker=0x7f020009;
+        public static final int last_updated_markers=0x7f02000a;
+        public static final int praveen=0x7f02000b;
+        public static final int sampath=0x7f02000c;
+        public static final int sudheer=0x7f02000d;
     }
     public static final class id {
-        public static final int about=0x7f080008;
-        public static final int credits=0x7f080009;
+        public static final int about=0x7f080009;
+        public static final int changelog=0x7f080008;
+        public static final int credits=0x7f08000a;
+        public static final int help=0x7f08000b;
         public static final int image=0x7f080000;
         public static final int lastUpdatedView=0x7f080006;
-        public static final int mapview=0x7f080007;
+        public static final int map=0x7f080007;
         public static final int parentlayout=0x7f080002;
         public static final int progressBar=0x7f080004;
         public static final int statusMessage=0x7f080005;
@@ -41,8 +46,10 @@ public final class R {
     }
     public static final class layout {
         public static final int about=0x7f030000;
-        public static final int activity_main=0x7f030001;
+        public static final int change_log=0x7f030001;
         public static final int credits=0x7f030002;
+        public static final int help=0x7f030003;
+        public static final int main=0x7f030004;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;
@@ -51,24 +58,67 @@ public final class R {
         public static final int about=0x7f050003;
         /**  About 
          */
-        public static final int aboutTTT=0x7f05000d;
+        public static final int aboutTTT=0x7f05001b;
         public static final int app_name=0x7f050000;
+        public static final int changelog=0x7f050005;
         public static final int credits=0x7f050002;
-        public static final int issues=0x7f050010;
-        public static final int issuesHeading=0x7f05000f;
-        public static final int jithu=0x7f050009;
-        public static final int jithuWork=0x7f05000a;
-        public static final int praveen=0x7f05000b;
-        public static final int praveenWork=0x7f05000c;
-        public static final int sampath=0x7f050007;
-        public static final int sampathWork=0x7f050008;
-        public static final int statusMessage=0x7f050004;
+        public static final int help=0x7f050004;
+        public static final int help_black_marker=0x7f050024;
+        public static final int help_blue_marker=0x7f050020;
+        public static final int help_bus_stop=0x7f050025;
+        public static final int help_green_marker=0x7f050021;
+        public static final int help_last_updated=0x7f050026;
+        public static final int help_last_updated_marker=0x7f050028;
+        public static final int help_last_updated_markers=0x7f050027;
+        /**  Help 
+         */
+        public static final int help_marker_colors=0x7f05001f;
+        public static final int help_markers_update_info=0x7f05002a;
+        public static final int help_notice=0x7f05002b;
+        public static final int help_notice_text=0x7f05002c;
+        public static final int help_other_info=0x7f050029;
+        public static final int help_red_marker=0x7f050022;
+        public static final int help_yellow_marker=0x7f050023;
+        public static final int issues=0x7f05001e;
+        public static final int issuesHeading=0x7f05001d;
+        public static final int jithu=0x7f050017;
+        public static final int jithuWork=0x7f050018;
+        public static final int praveen=0x7f050019;
+        public static final int praveenWork=0x7f05001a;
+        public static final int sampath=0x7f050015;
+        public static final int sampathWork=0x7f050016;
+        public static final int statusMessage=0x7f050006;
         /**  credits 
          */
-        public static final int sudheer=0x7f050005;
-        public static final int sudheerWork=0x7f050006;
+        public static final int sudheer=0x7f050013;
+        public static final int sudheerWork=0x7f050014;
         public static final int title_activity_main=0x7f050001;
-        public static final int version=0x7f05000e;
+        public static final int version=0x7f05001c;
+        /**  V1.0 Alpha 
+         */
+        public static final int version1_0Heading=0x7f050011;
+        public static final int version1_0Log=0x7f050012;
+        /**  V2.0 Beta 
+         */
+        public static final int version2_0Heading=0x7f05000f;
+        public static final int version2_0Log=0x7f050010;
+        /**  V2.1 Beta 
+         */
+        public static final int version2_1Heading=0x7f05000d;
+        public static final int version2_1Log=0x7f05000e;
+        /**  V3.0 RP 
+         */
+        public static final int version3_0Heading=0x7f05000b;
+        public static final int version3_0Log=0x7f05000c;
+        /**  V3.1 RP 
+         */
+        public static final int version3_1Heading=0x7f050009;
+        public static final int version3_1Log=0x7f05000a;
+        /**  Change log 
+ V3.2 RP 
+         */
+        public static final int version3_2Heading=0x7f050007;
+        public static final int version3_2Log=0x7f050008;
     }
     public static final class style {
         /** 

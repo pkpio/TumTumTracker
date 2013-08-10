b8dab4ad99131b22febadf24a120f15d77e89ad1
Praveen Kumar Pendyala <praveendath92@gmail.com>
2013-08-11 05:29:55 +0530

    Version 3.3 Release preview
    Added support to Gingerbread and Froyo. Ignore the early commit name. It
    was written wrong

--- src/in/co/praveenkumar/tumtumtracker/JSONParser.java
+++ src/in/co/praveenkumar/tumtumtracker/JSONParser.java
@@ -9,7 +9,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 
 import org.apache.http.HttpEntity;

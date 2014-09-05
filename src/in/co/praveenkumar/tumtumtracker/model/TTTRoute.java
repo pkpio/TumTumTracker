5987b45bc745a81073bbff41252ebe80fbc906f7
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-09-05 19:47:58 +0530

    Route model initialized

---
+++ src/in/co/praveenkumar/tumtumtracker/model/TTTRoute.java
@@ -0,0 +1,13 @@
+package in.co.praveenkumar.tumtumtracker.model;
+
+import com.orm.SugarRecord;
+
+/**
+ * TTT route object
+ * 
+ * @author Praveen Kumar Pendyala (praveen@praveenkumar.co.in)
+ * 
+ */
+public class TTTRoute extends SugarRecord<TTTRoute> {
+
+}

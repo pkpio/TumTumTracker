b32baafb5236c083ea407097fcf38582c4d39ba0
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-09-05 22:02:50 +0530

    Init of Session.response done

--- src/in/co/praveenkumar/tumtumtracker/helper/Session.java
+++ src/in/co/praveenkumar/tumtumtracker/helper/Session.java
@@ -1,8 +1,37 @@
 package in.co.praveenkumar.tumtumtracker.helper;
 
+import java.util.List;
+
+import in.co.praveenkumar.tumtumtracker.model.TTTMarker;
 import in.co.praveenkumar.tumtumtracker.model.TTTSiteResponse;
 
 public class Session {
 	public static TTTSiteResponse response;
+
+	/**
+	 * Initializes the Session.response with data from Sql db. Returns false
+	 * when init failed. Generally, Init fails if Sync never happened before or
+	 * the last sync has no markers.
+	 * 
+	 * @return initStatus
+	 */
+	public static Boolean init() {
+		List<TTTSiteResponse> responses = TTTSiteResponse
+				.listAll(TTTSiteResponse.class);
+		if (responses == null)
+			return false;
+		if (responses.size() == 0)
+			return false;
+		response = responses.get(0);
+
+		List<TTTMarker> markers = TTTMarker.listAll(TTTMarker.class);
+		if (markers == null)
+			return false;
+		if (markers.size() == 0)
+			return false;
+
+		response.setMarkers(markers);
+		return true;
+	}
 
 }

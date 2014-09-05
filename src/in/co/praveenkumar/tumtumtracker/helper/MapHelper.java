9211844a08b3b288465813495165cabfc7512985
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-09-05 22:55:13 +0530

    MapHelper to get icons for markers added

---
+++ src/in/co/praveenkumar/tumtumtracker/helper/MapHelper.java
@@ -0,0 +1,39 @@
+package in.co.praveenkumar.tumtumtracker.helper;
+
+import in.co.praveenkumar.tumtumtracker.R;
+
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+
+public class MapHelper {
+
+	public static BitmapDescriptor MarkerIcon(int type) {
+		switch (type) {
+		case 1:
+			return BitmapDescriptorFactory.fromResource(R.drawable.bus_blue);
+		case 2:
+			return BitmapDescriptorFactory.fromResource(R.drawable.bus_green);
+		case 3:
+			return BitmapDescriptorFactory
+					.fromResource(R.drawable.bus_lightblue);
+		case 4:
+			return BitmapDescriptorFactory.fromResource(R.drawable.bus_lime);
+		case 5:
+			return BitmapDescriptorFactory.fromResource(R.drawable.bus_brown);
+		case 6:
+			return BitmapDescriptorFactory.fromResource(R.drawable.bus_orange);
+		case 7:
+			return BitmapDescriptorFactory.fromResource(R.drawable.bus_pink);
+		case 8:
+			return BitmapDescriptorFactory.fromResource(R.drawable.bus_purple);
+		case 9:
+			return BitmapDescriptorFactory.fromResource(R.drawable.bus_red);
+		case 10:
+			return BitmapDescriptorFactory.fromResource(R.drawable.bus_stop);
+		default:
+			return BitmapDescriptorFactory.fromResource(R.drawable.bus_yellow);
+		}
+
+	}
+
+}

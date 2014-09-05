c6177c87477acd29aa11baf2c0e4738f0214c57d
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-09-05 20:40:04 +0530

    Added param and session classes

---
+++ src/in/co/praveenkumar/tumtumtracker/helper/Param.java
@@ -0,0 +1,14 @@
+package in.co.praveenkumar.tumtumtracker.helper;
+
+public class Param {
+	/**
+	 * URL for feteching json data
+	 */
+	public static String dataUrl = "http://tumtum-iitb.org/ttt_data/";
+
+	/**
+	 * Data update frequency in milliseconds
+	 */
+	public static int frequency = 3000;
+
+}
---
+++ src/in/co/praveenkumar/tumtumtracker/helper/Session.java
@@ -0,0 +1,8 @@
+package in.co.praveenkumar.tumtumtracker.helper;
+
+import in.co.praveenkumar.tumtumtracker.model.TTTSiteResponse;
+
+public class Session {
+	public static TTTSiteResponse response;
+
+}

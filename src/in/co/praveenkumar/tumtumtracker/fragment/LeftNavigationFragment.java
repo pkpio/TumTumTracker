1ac5eba9b2c5c48144dac5221d4bdc6c80d8e217
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-09-05 18:54:15 +0530

    Map setup and left drawer setup complete Working

---
+++ src/in/co/praveenkumar/tumtumtracker/fragment/LeftNavigationFragment.java
@@ -0,0 +1,21 @@
+package in.co.praveenkumar.tumtumtracker.fragment;
+
+import in.co.praveenkumar.tumtumtracker.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LeftNavigationFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.frag_left_navigation,
+				container, false);
+		return rootView;
+	}
+
+}

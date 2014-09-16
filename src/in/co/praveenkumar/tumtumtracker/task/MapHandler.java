cf065d82c51a4abf9401b1b06ed416ec470a77e0
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-09-16 10:17:50 +0530

    Sample route plotting added

--- src/in/co/praveenkumar/tumtumtracker/task/MapHandler.java
+++ src/in/co/praveenkumar/tumtumtracker/task/MapHandler.java
@@ -6,22 +6,26 @@ import in.co.praveenkumar.tumtumtracker.helper.Param;
 import in.co.praveenkumar.tumtumtracker.helper.Session;
 import in.co.praveenkumar.tumtumtracker.model.TTTMarker;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.support.v4.app.FragmentManager;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 @SuppressLint("UseSparseArrays")
 public class MapHandler {
@@ -59,6 +63,19 @@ public class MapHandler {
 				marker.showInfoWindow();
 			hashMap.put(mark.getMarkerid(), marker.getId());
 		}
+
+		// Testing poly lines
+		List<LatLng> list = decodePoly("ynvsBqm`|LG@m@VC@e@Pw@ZiAb@s@VgAXs@LYBWB_AB_@?O?{BGsA?w@?o@@");
+
+		for (int z = 0; z < list.size() - 1; z++) {
+			LatLng src = list.get(z);
+			LatLng dest = list.get(z + 1);
+			Polyline line = mMap.addPolyline(new PolylineOptions()
+					.add(new LatLng(src.latitude, src.longitude),
+							new LatLng(dest.latitude, dest.longitude)).width(2)
+					.color(Color.BLUE).geodesic(true));
+		}
+		// line.setPoints(points);
 	}
 
 	private void setUpMapIfNeeded() {
@@ -94,7 +111,6 @@ public class MapHandler {
 				.tilt(Param.tilt).build();
 		mMap.animateCamera(CameraUpdateFactory
 				.newCameraPosition(cameraPosition));
-
 	}
 
 	OnMarkerClickListener ttClickListener = new OnMarkerClickListener() {
@@ -122,5 +138,41 @@ public class MapHandler {
 			}
 		}
 		return null;
+	}
+
+	// -TODO-testing code
+	public List<LatLng> decodePoly(String encoded) {
+
+		List<LatLng> poly = new ArrayList<LatLng>();
+		int index = 0, len = encoded.length();
+		int lat = 0, lng = 0;
+
+		while (index < len) {
+			int b, shift = 0, result = 0;
+			do {
+				b = encoded.charAt(index++) - 63;
+				result |= (b & 0x1f) << shift;
+				shift += 5;
+			} while (b >= 0x20);
+			int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+			lat += dlat;
+
+			shift = 0;
+			result = 0;
+			do {
+				b = encoded.charAt(index++) - 63;
+				result |= (b & 0x1f) << shift;
+				shift += 5;
+			} while (b >= 0x20);
+			int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+			lng += dlng;
+
+			LatLng p = new LatLng((((double) lat / 1E5)),
+					(((double) lng / 1E5)));
+			poly.add(p);
+		}
+
+		return poly;
+
 	}
 }

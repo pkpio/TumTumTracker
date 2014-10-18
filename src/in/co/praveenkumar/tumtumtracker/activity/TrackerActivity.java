a7a0cb23122e84abe7c49790fb4075213910003e
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-10-18 22:20:24 +0530

    Routes setup added

--- src/in/co/praveenkumar/tumtumtracker/activity/TrackerActivity.java
+++ src/in/co/praveenkumar/tumtumtracker/activity/TrackerActivity.java
@@ -3,27 +3,46 @@ package in.co.praveenkumar.tumtumtracker.activity;
 import in.co.praveenkumar.tumtumtracker.R;
 import in.co.praveenkumar.tumtumtracker.adapter.AppNavigationDrawer;
 import in.co.praveenkumar.tumtumtracker.dialog.LoadingMessage;
+import in.co.praveenkumar.tumtumtracker.helper.GsonExclude;
 import in.co.praveenkumar.tumtumtracker.helper.Param;
+import in.co.praveenkumar.tumtumtracker.model.TTTOverviewPoly;
+import in.co.praveenkumar.tumtumtracker.model.TTTRoute;
+import in.co.praveenkumar.tumtumtracker.model.TTTRouteResponse;
 import in.co.praveenkumar.tumtumtracker.task.MapHandler;
 import in.co.praveenkumar.tumtumtracker.task.MarkerSync;
+
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class TrackerActivity extends AppNavigationDrawer {
+	Context context;
 	MapHandler mapHandler;
 	LoadingMessage loadMessage;
+	int fails = 0;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		this.context = this;
+		setContentView(R.layout.activity_tracker);
 
 		// Loading message setup
 		loadMessage = new LoadingMessage(this);
 		loadMessage.show();
 
-		setContentView(R.layout.activity_tracker);
 		setUpDrawer();
+		setupRoutes();
 
 		// MapHandler setup
 		mapHandler = new MapHandler(getSupportFragmentManager());
@@ -48,15 +67,27 @@ public class TrackerActivity extends AppNavigationDrawer {
 		@Override
 		protected void onPostExecute(Boolean syncStatus) {
 			if (syncStatus) {
+				fails = 0;
 				mapHandler.overlayMarkers();
 				loadMessage.dismiss();
+
+				// Start next update after some wait.
+				Handler myHandler = new Handler();
+				myHandler.postDelayed(syncLooper, Param.frequency);
+			} else {
+				fails++;
+				if (fails == 3) {
+					loadMessage.dismiss();
+					Toast.makeText(context,
+							"Updated failed. Data may not be real time!",
+							Toast.LENGTH_LONG).show();
+				}
+
+				// Start next attempt after some wait.
+				Handler myHandler = new Handler();
+				myHandler.postDelayed(syncLooper, Param.failWait);
 			}
-
-			// Start next update after some wait.
-			Handler myHandler = new Handler();
-			myHandler.postDelayed(syncLooper, Param.frequency);
 		}
-
 	}
 
 	/**
@@ -69,4 +100,38 @@ public class TrackerActivity extends AppNavigationDrawer {
 		}
 	};
 
+	void setupRoutes() {
+		List<TTTRoute> routes = TTTRoute.listAll(TTTRoute.class);
+		if (routes.size() != 0)
+			return;
+
+		AssetManager assetManager = context.getAssets();
+		try {
+			InputStreamReader reader = new InputStreamReader(
+					assetManager.open("routes.json"));
+			GsonExclude ex = new GsonExclude();
+			Gson gson = new GsonBuilder()
+					.addDeserializationExclusionStrategy(ex)
+					.addSerializationExclusionStrategy(ex).create();
+			TTTRouteResponse response = gson.fromJson(reader,
+					TTTRouteResponse.class);
+			reader.close();
+
+			routes = response.getRoutes();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		List<TTTOverviewPoly> polylines = new ArrayList<TTTOverviewPoly>();
+
+		for (int i = 0; i < routes.size(); i++) {
+			TTTRoute route = routes.get(i);
+			route.save();
+			polylines = route.getOverviewpolylines();
+			for (int j = 0; j < polylines.size(); j++) {
+				polylines.get(j).setParentid(route.getId());
+				polylines.get(j).save();
+			}
+		}
+	}
 }

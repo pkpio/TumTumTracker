b7a10a8b72f275e4c126ef3886145d1eef508f5f
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-10-18 11:51:01 +0530

    Route model added

--- src/in/co/praveenkumar/tumtumtracker/model/TTTRoute.java
+++ src/in/co/praveenkumar/tumtumtracker/model/TTTRoute.java
@@ -1,5 +1,7 @@
 package in.co.praveenkumar.tumtumtracker.model;
 
+import java.util.List;
+
 import com.orm.SugarRecord;
 
 /**
@@ -9,5 +11,51 @@ import com.orm.SugarRecord;
  * 
  */
 public class TTTRoute extends SugarRecord<TTTRoute> {
+	int routeid;
+	String title;
+	String description;
+	int color;
+	List<TTTOverviewPoly> overviewpolylines;
+
+	/**
+	 * Route id given by TTT site
+	 * 
+	 * @return routeid
+	 */
+	public int getRouteid() {
+		return routeid;
+	}
+
+	/**
+	 * Route title
+	 * 
+	 * @return title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * Route description
+	 * 
+	 * @return description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * Color code integer. Get actual color value from getColor(colorCode)
+	 * method.
+	 * 
+	 * @return colorcode
+	 */
+	public int getColor() {
+		return color;
+	}
+
+	public List<TTTOverviewPoly> getOverviewpolylines() {
+		return overviewpolylines;
+	}
 
 }

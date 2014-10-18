c9d8360e1a09c2679509dc28951b74e47c46d3ea
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-10-19 00:37:33 +0530

    Added RoutePlotter interface

--- src/in/co/praveenkumar/tumtumtracker/AppInterface.java
+++ src/in/co/praveenkumar/tumtumtracker/AppInterface.java
@@ -1,5 +1,21 @@
 package in.co.praveenkumar.tumtumtracker;
 
+import in.co.praveenkumar.tumtumtracker.model.TTTRoute;
+
 public class AppInterface {
-	
+	/**
+	 * Gives an interface for plotting a route selected from left navigation
+	 * 
+	 * @author Praveen Kumar Pendyala (praveen@praveenkumar.co.in)
+	 * 
+	 */
+	public interface RoutePlotter {
+		/**
+		 * Plot the route on map.
+		 * 
+		 * @param Route
+		 *            Route to be plotted
+		 */
+		public void plotRoute(TTTRoute route);
+	}
 }

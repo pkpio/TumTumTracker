ab26f4c06099fbcfdc71a1a760de02649a75bae9
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-10-19 00:24:27 +0530

    Added routePlot method to MapHandler

--- src/in/co/praveenkumar/tumtumtracker/task/MapHandler.java
+++ src/in/co/praveenkumar/tumtumtracker/task/MapHandler.java
@@ -5,6 +5,8 @@ import in.co.praveenkumar.tumtumtracker.helper.MapHelper;
 import in.co.praveenkumar.tumtumtracker.helper.Param;
 import in.co.praveenkumar.tumtumtracker.helper.Session;
 import in.co.praveenkumar.tumtumtracker.model.TTTMarker;
+import in.co.praveenkumar.tumtumtracker.model.TTTOverviewPoly;
+import in.co.praveenkumar.tumtumtracker.model.TTTRoute;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -24,12 +26,14 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 @SuppressLint("UseSparseArrays")
 public class MapHandler {
 	FragmentManager mFragmentManager;
 	GoogleMap mMap;
+	List<Polyline> mRoute;
 
 	// Last open windows detection
 	HashMap<Integer, String> hashMap = new HashMap<Integer, String>();
@@ -62,97 +66,46 @@ public class MapHandler {
 				marker.showInfoWindow();
 			hashMap.put(mark.getMarkerid(), marker.getId());
 		}
+	}
 
-		// Testing poly lines
-		List<LatLng> list = decodePoly("covsBqm`|L@@e@Ro@V_A\\]Ne@P{Bp@{@Ni@D_AB_@?}AEq@AmA?wIF_@Bm@Ja@Ne@NmAp@]X]n@MZWdAMl@");
-		List<LatLng> list2 = decodePoly("aqvsBe__|LXNZJVFT@P?b@GVI^OZQJINUFYFa@Dc@D[@_@@U@S?]Ak@A]Ek@Gq@Ea@M]Yk@Sc@GQCe@Ei@Cm@@QFQFMJKLM??FO@UKSy@i@QKAa@Ak@G{@A[@e@Da@Fi@]aA_@a@aAa@cHjCsE\\EgC");
-		List<LatLng> list3 = decodePoly("g|wsB{}_|L}FnU");
-		List<LatLng> list4 = decodePoly("edxsBkg_|L}@jDa@~AYpAUz@]fBYhBYtCYzB");
-		List<LatLng> list5 = decodePoly("edxsBkg_|LqJoAPuDI_DBu@^qDByAMoBMw@");
-		List<LatLng> list6 = decodePoly("coxsB{c`|L~GxAzHdB");
-		List<LatLng> list7 = decodePoly("coxsB{c`|L_Dm@cCQ");
-		List<LatLng> list8 = decodePoly("gxxsB{e`|LmCwAQMIOCU@UJ_AH_@Ne@LYzAyBnB{B^Ul@G`JEZAJCDGFGFMFG^OzEFF@@B@BLhABBXF|Dc@JCFo@Cq@@OFQHMJKLGFAzJ[RA");
-		List<LatLng> list9 = decodePoly("ugwsBy~`|LThO@vA?Z?zA");
+	/**
+	 * Plots a route on the map. This clears any route plot that already on map.
+	 * 
+	 * @param route
+	 */
+	public void drawRoute(TTTRoute route) {
+		System.out.println("Plotting route");
+		if (route == null)
+			return;
 
-		for (int z = 0; z < list.size() - 1; z++) {
-			LatLng src = list.get(z);
-			LatLng dest = list.get(z + 1);
-			mMap.addPolyline(new PolylineOptions()
-					.add(new LatLng(src.latitude, src.longitude),
-							new LatLng(dest.latitude, dest.longitude)).width(2)
-					.color(Color.BLUE).geodesic(true));
-		}
+		List<TTTOverviewPoly> polylines = route.getOverviewpolylines();
+		if (polylines == null)
+			return;
 
-		for (int z = 0; z < list2.size() - 1; z++) {
-			LatLng src = list2.get(z);
-			LatLng dest = list2.get(z + 1);
-			mMap.addPolyline(new PolylineOptions()
-					.add(new LatLng(src.latitude, src.longitude),
-							new LatLng(dest.latitude, dest.longitude)).width(2)
-					.color(Color.BLUE).geodesic(true));
-		}
+		// Clear existing route plot
+		if (mRoute != null)
+			for (int i = 0; i < mRoute.size(); i++)
+				mRoute.get(i).remove();
+		mRoute = new ArrayList<Polyline>();
 
-		for (int z = 0; z < list3.size() - 1; z++) {
-			LatLng src = list3.get(z);
-			LatLng dest = list3.get(z + 1);
-			mMap.addPolyline(new PolylineOptions()
-					.add(new LatLng(src.latitude, src.longitude),
-							new LatLng(dest.latitude, dest.longitude)).width(2)
-					.color(Color.BLUE).geodesic(true));
-		}
+		// Loop through each path in the route
+		for (int i = 0; i < polylines.size(); i++) {
 
-		for (int z = 0; z < list4.size() - 1; z++) {
-			LatLng src = list4.get(z);
-			LatLng dest = list4.get(z + 1);
-			mMap.addPolyline(new PolylineOptions()
-					.add(new LatLng(src.latitude, src.longitude),
-							new LatLng(dest.latitude, dest.longitude)).width(2)
-					.color(Color.BLUE).geodesic(true));
-		}
+			// Get points that make up a path
+			List<LatLng> points = decodePoly(polylines.get(i).getLine());
 
-		for (int z = 0; z < list5.size() - 1; z++) {
-			LatLng src = list5.get(z);
-			LatLng dest = list5.get(z + 1);
-			mMap.addPolyline(new PolylineOptions()
-					.add(new LatLng(src.latitude, src.longitude),
-							new LatLng(dest.latitude, dest.longitude)).width(2)
-					.color(Color.BLUE).geodesic(true));
-		}
+			// Plot each point as a polyline
+			for (int z = 0; z < points.size() - 1; z++) {
+				LatLng src = points.get(z);
+				LatLng dest = points.get(z + 1);
+				Polyline line = mMap.addPolyline(new PolylineOptions()
+						.add(new LatLng(src.latitude, src.longitude),
+								new LatLng(dest.latitude, dest.longitude))
+						.width(2).color(Color.BLUE).geodesic(true));
 
-		for (int z = 0; z < list6.size() - 1; z++) {
-			LatLng src = list6.get(z);
-			LatLng dest = list6.get(z + 1);
-			mMap.addPolyline(new PolylineOptions()
-					.add(new LatLng(src.latitude, src.longitude),
-							new LatLng(dest.latitude, dest.longitude)).width(2)
-					.color(Color.BLUE).geodesic(true));
-		}
-
-		for (int z = 0; z < list7.size() - 1; z++) {
-			LatLng src = list7.get(z);
-			LatLng dest = list7.get(z + 1);
-			mMap.addPolyline(new PolylineOptions()
-					.add(new LatLng(src.latitude, src.longitude),
-							new LatLng(dest.latitude, dest.longitude)).width(2)
-					.color(Color.BLUE).geodesic(true));
-		}
-
-		for (int z = 0; z < list8.size() - 1; z++) {
-			LatLng src = list8.get(z);
-			LatLng dest = list8.get(z + 1);
-			mMap.addPolyline(new PolylineOptions()
-					.add(new LatLng(src.latitude, src.longitude),
-							new LatLng(dest.latitude, dest.longitude)).width(2)
-					.color(Color.BLUE).geodesic(true));
-		}
-
-		for (int z = 0; z < list9.size() - 1; z++) {
-			LatLng src = list9.get(z);
-			LatLng dest = list9.get(z + 1);
-			mMap.addPolyline(new PolylineOptions()
-					.add(new LatLng(src.latitude, src.longitude),
-							new LatLng(dest.latitude, dest.longitude)).width(2)
-					.color(Color.BLUE).geodesic(true));
+				// Add this to the list of points in route.
+				mRoute.add(line);
+			}
 		}
 	}
 
@@ -218,7 +171,6 @@ public class MapHandler {
 		return null;
 	}
 
-	// -TODO-testing code
 	public List<LatLng> decodePoly(String encoded) {
 
 		List<LatLng> poly = new ArrayList<LatLng>();

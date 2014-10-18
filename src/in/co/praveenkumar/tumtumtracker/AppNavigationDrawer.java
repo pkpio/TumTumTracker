408760640df6e6e128194cb2ee9236b1c8f5354a
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-10-19 00:22:43 +0530

    Route init and async route loader in LeftNav fragment

--- src/in/co/praveenkumar/tumtumtracker/AppNavigationDrawer.java
+++ src/in/co/praveenkumar/tumtumtracker/AppNavigationDrawer.java
@@ -1,6 +1,5 @@
 package in.co.praveenkumar.tumtumtracker;
 
-import in.co.praveenkumar.tumtumtracker.R;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;

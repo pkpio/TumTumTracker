f433647b46e94c7161f3e12df2dbf3aeaa824cea
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-10-19 00:04:03 +0530

    Serialize tag set in OverviewPoly model

--- src/in/co/praveenkumar/tumtumtracker/model/TTTOverviewPoly.java
+++ src/in/co/praveenkumar/tumtumtracker/model/TTTOverviewPoly.java
@@ -1,18 +1,21 @@
 package in.co.praveenkumar.tumtumtracker.model;
 
+import com.google.gson.annotations.SerializedName;
 import com.orm.SugarRecord;
 
 public class TTTOverviewPoly extends SugarRecord<TTTOverviewPoly> {
 	long parentid;
-	String data;
+
+	@SerializedName("line")
+	String line;
 
 	/**
 	 * Get overviewpoly string
 	 * 
 	 * @return overviewpolyline
 	 */
-	public String getData() {
-		return data;
+	public String getLine() {
+		return line;
 	}
 
 	/**

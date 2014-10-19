cb8d566b33a5b42e7eb817738d2aa0b5e75d0279
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-10-19 15:28:43 +0530

    Params and helper icon updated

--- src/in/co/praveenkumar/tumtumtracker/helper/MapHelper.java
+++ src/in/co/praveenkumar/tumtumtracker/helper/MapHelper.java
@@ -14,14 +14,14 @@ public class MapHelper {
 		case 2:
 			return BitmapDescriptorFactory.fromResource(R.drawable.bus_green);
 		case 3:
-			return BitmapDescriptorFactory
-					.fromResource(R.drawable.bus_lightblue);
+			return BitmapDescriptorFactory.fromResource(R.drawable.bus_orange);
 		case 4:
 			return BitmapDescriptorFactory.fromResource(R.drawable.bus_lime);
 		case 5:
 			return BitmapDescriptorFactory.fromResource(R.drawable.bus_brown);
 		case 6:
-			return BitmapDescriptorFactory.fromResource(R.drawable.bus_orange);
+			return BitmapDescriptorFactory
+					.fromResource(R.drawable.bus_lightblue);
 		case 7:
 			return BitmapDescriptorFactory.fromResource(R.drawable.bus_pink);
 		case 8:

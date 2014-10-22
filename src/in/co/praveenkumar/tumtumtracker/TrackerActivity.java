c21959e0d50b3a16f568682e5d171e21636f0be2
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-10-22 18:38:15 +0530

    Better sync thread handling without handlers. Resolved #3

--- src/in/co/praveenkumar/tumtumtracker/TrackerActivity.java
+++ src/in/co/praveenkumar/tumtumtracker/TrackerActivity.java
@@ -1,16 +1,12 @@
 package in.co.praveenkumar.tumtumtracker;
 
-import java.util.List;
-
 import in.co.praveenkumar.tumtumtracker.AppInterface.RoutePlotter;
 import in.co.praveenkumar.tumtumtracker.helper.Param;
-import in.co.praveenkumar.tumtumtracker.model.TTTMarker;
 import in.co.praveenkumar.tumtumtracker.model.TTTRoute;
 import in.co.praveenkumar.tumtumtracker.task.MarkerSync;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.widget.Toast;
 
 public class TrackerActivity extends AppNavigationDrawer implements
@@ -19,6 +15,8 @@ public class TrackerActivity extends AppNavigationDrawer implements
 	MapHandler mapHandler;
 	LoadingMessageDialog loadMessage;
 	int fails = 0;
+	Boolean stopSync = false;
+	AsyncMarkerSync markerSync;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -34,9 +32,6 @@ public class TrackerActivity extends AppNavigationDrawer implements
 
 		// MapHandler setup
 		mapHandler = new MapHandler(this);
-
-		// Start marker sync
-		new AsyncMarkerSync().execute("");
 	}
 
 	/**
@@ -44,17 +39,33 @@ public class TrackerActivity extends AppNavigationDrawer implements
 	 * 
 	 * @author Praveen Kumar Pendyala (praveen@praveenkumar.co.in)
 	 */
-	private class AsyncMarkerSync extends AsyncTask<String, Integer, Boolean> {
+	private class AsyncMarkerSync extends AsyncTask<String, Boolean, Boolean> {
 
 		@Override
 		protected Boolean doInBackground(String... url) {
-			MarkerSync ms = new MarkerSync();
-			return ms.syncMarkers();
+			while (true) {
+				MarkerSync ms = new MarkerSync();
+				publishProgress(ms.syncMarkers());
+
+				// Stop sync if it needs to be.
+				if (stopSync)
+					return true;
+
+				// Start next update after some wait.
+				try {
+					if (fails == 0)
+						Thread.sleep(Param.frequency);
+					else
+						Thread.sleep(Param.failWait);
+				} catch (InterruptedException e) {
+					System.out.println("Marker sync sleep interrupted!");
+				}
+			}
 		}
 
 		@Override
-		protected void onPostExecute(Boolean syncStatus) {
-			if (syncStatus)
+		protected void onProgressUpdate(Boolean... progress) {
+			if (progress[0])
 				fails = 0;
 			else
 				fails++;
@@ -68,33 +79,46 @@ public class TrackerActivity extends AppNavigationDrawer implements
 				loadMessage.dismiss();
 
 			mapHandler.overlayMarkers();
-
-			// Start next update after some wait.
-			Handler myHandler = new Handler();
-			if (fails == 0)
-				myHandler.postDelayed(syncLooper, Param.frequency);
-			else
-				myHandler.postDelayed(syncLooper, Param.failWait);
-
-			List<TTTMarker> markers = TTTMarker.listAll(TTTMarker.class);
-			if (markers != null)
-				System.out.println("Marker size: " + markers.size());
 		}
 	}
-
-	/**
-	 * A runnable to support looping sync task.
-	 */
-	private Runnable syncLooper = new Runnable() {
-		@Override
-		public void run() {
-			new AsyncMarkerSync().execute("");
-		}
-	};
 
 	@Override
 	public void plotRoute(TTTRoute route) {
 		mapHandler.drawRoute(route);
 		this.getLayoutInflater();
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		startSync();
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		stopSync();
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		stopSync();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		stopSync();
+	}
+
+	void stopSync() {
+		stopSync = true;
+	}
+
+	void startSync() {
+		stopSync = false;
+		markerSync = new AsyncMarkerSync();
+		markerSync.execute("");
 	}
 }

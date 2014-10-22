cbcebad7e98d9d13da03da0b1b8c0738fc8a957f
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-10-22 18:08:44 +0530

    Updated relativeTime function. Resolved #2

--- src/in/co/praveenkumar/tumtumtracker/helper/TimeFormat.java
+++ src/in/co/praveenkumar/tumtumtracker/helper/TimeFormat.java
@@ -1,22 +1,21 @@
 package in.co.praveenkumar.tumtumtracker.helper;
 
-import java.util.Calendar;
-
 public class TimeFormat {
+	static final int SECOND = 1;
+	static final int MINUTE = 60 * SECOND;
+	static final int HOUR = 60 * MINUTE;
+	static final int DAY = 24 * HOUR;
+	static final int WEEK = 7 * DAY;
+	static final int MONTH = 4 * WEEK;
+	static final int YEAR = 12 * MONTH;
+
 	/**
 	 * Evaluates relative time for given time stamp. The outputs could looks
 	 * like,<br/>
-	 * <b> For past </b><br/>
 	 * 2 years ago <br/>
 	 * 3 months ago <br/>
 	 * 5 minutes ago <br/>
 	 * A moment ago <br/>
-	 * <br/>
-	 * <b> For furture </b><br/>
-	 * In 2 years<br/>
-	 * In 3 months<br/>
-	 * In 5 minutes<br/>
-	 * In a moment<br/>
 	 * <br/>
 	 * 
 	 * <b>Note:</b> Plural and singular of the numbers are also considered. Like
@@ -26,91 +25,46 @@ public class TimeFormat {
 	 *            in seconds
 	 * @return neat relative time string
 	 */
-	public static String getNiceRelativeTime(int time) {
-		long ltime = ((long) time) * 1000;
-		Calendar c = Calendar.getInstance();
-		int nowSec = c.get(Calendar.SECOND);
-		int nowMin = c.get(Calendar.MINUTE);
-		int nowHour = c.get(Calendar.HOUR_OF_DAY);
-		int nowDay = c.get(Calendar.DATE);
-		int nowMonth = c.get(Calendar.MONTH);
-		int nowYear = c.get(Calendar.YEAR);
+	public static String RelativeTime(int time) {
+		int diff = (int) (System.currentTimeMillis() / 1000) - time;
 
-		c.setTimeInMillis(ltime);
-		int givenSec = c.get(Calendar.SECOND);
-		int givenMin = c.get(Calendar.MINUTE);
-		int givenHour = c.get(Calendar.HOUR_OF_DAY);
-		int givenDay = c.get(Calendar.DATE);
-		int givenMonth = c.get(Calendar.MONTH);
-		int givenYear = c.get(Calendar.YEAR);
+		if (diff < 10 * SECOND)
+			return "A moment ago";
 
-		// Past
-		if (time * 1000 < System.currentTimeMillis()) {
-			if (nowYear - givenYear > 1)
-				return (nowYear - givenYear) + " years ago";
-			else if (nowYear - givenYear == 1)
-				return "1 year ago";
+		if (diff < 20 * SECOND)
+			return "Few seconds ago";
 
-			else if (nowMonth - givenMonth > 1)
-				return (nowMonth - givenMonth) + " months ago";
-			else if (nowMonth - givenMonth == 1)
-				return "1 month ago";
+		if (diff < 45 * SECOND)
+			return "30 seconds ago";
 
-			else if (nowDay - givenDay > 1)
-				return (nowDay - givenDay) + " days ago";
-			else if (nowDay - givenDay == 1)
-				return "Yesterday";
+		if (diff < 90 * SECOND)
+			return "A minute ago";
 
-			else if (nowHour - givenHour > 1)
-				return (nowHour - givenHour) + " hours ago";
-			else if (nowHour - givenHour == 1)
-				return "1 hour ago";
+		if (diff < 60 * MINUTE)
+			return (int) Math.ceil(diff / MINUTE) + " minutes ago";
 
-			else if (nowMin - givenMin > 1)
-				return (nowMin - givenMin) + " minutes ago";
-			else if (nowMin - givenMin == 1)
-				return "1 minute ago";
+		if (diff < 90 * MINUTE)
+			return "An hour ago";
 
-			else if (nowSec - givenSec > 10)
-				return (nowSec - givenSec) + " seconds ago";
+		if (diff < 23 * HOUR)
+			return (int) Math.ceil(diff / HOUR) + " hours ago";
 
-			else
-				return "A moment ago";
-		}
+		if (diff < 30 * HOUR)
+			return "Yesterday";
 
-		// Future
-		else {
-			if (givenYear - nowYear > 1)
-				return "In " + (givenYear - nowYear) + " years";
-			else if (givenYear - nowYear == 1)
-				return "In 1 year";
+		if (diff < 28 * DAY)
+			return (int) Math.ceil(diff / DAY) + " days ago";
 
-			else if (givenMonth - nowMonth > 1)
-				return "In " + (givenMonth - nowMonth) + " months";
-			else if (givenMonth - nowMonth == 1)
-				return "In 1 month";
+		if (diff < 40 * DAY)
+			return "A month ago";
 
-			else if (givenDay - nowDay > 1)
-				return "In " + (givenDay - nowDay) + " days";
-			else if (givenDay - nowDay == 1)
-				return "Tomorrow";
+		if (diff < 12 * MONTH)
+			return (int) Math.ceil(diff / MONTH) + " months ago";
 
-			else if (givenHour - nowHour > 1)
-				return "In " + (givenHour - nowHour) + " hours";
-			else if (givenHour - nowHour == 1)
-				return "In 1 hour";
+		if (diff < 16 * MONTH)
+			return "An year ago";
 
-			else if (givenMin - nowMin > 1)
-				return "In " + (givenMin - nowMin) + " minutes";
-			else if (givenMin - nowMin == 1)
-				return "In 1 minute";
-
-			else if (givenSec - nowSec > 10)
-				return "In " + (givenSec - nowSec) + " seconds";
-
-			else
-				return "In a moment";
-		}
+		return (int) Math.ceil(diff / YEAR) + " years ago";
 
 	}
 

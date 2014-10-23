89f5a0c2547cbb47b67833ca64604bb2432c7bca
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-10-23 11:23:46 +0530

    Added new methods to mapHelper for route ops

--- src/in/co/praveenkumar/tumtumtracker/helper/MapHelper.java
+++ src/in/co/praveenkumar/tumtumtracker/helper/MapHelper.java
@@ -2,10 +2,18 @@ package in.co.praveenkumar.tumtumtracker.helper;
 
 import in.co.praveenkumar.tumtumtracker.R;
 
+import android.graphics.Color;
+
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 
 public class MapHelper {
+	static final String ORANGE = "#f25f0f";
+	static final String BLUE = "#43adf1";
+	static final String RED = "#f10f00";
+	static final String YELLOW = "#f4b400";
+	static final String GREEN = "#2f8a78";
+	static final String BLACK = "#000000";
 
 	public static BitmapDescriptor MarkerIcon(int type) {
 		switch (type) {
@@ -34,6 +42,46 @@ public class MapHelper {
 			return BitmapDescriptorFactory.fromResource(R.drawable.bus_yellow);
 		}
 
+	}
+
+	public static int RouteColor(int color) {
+		switch (color) {
+		case 1:
+			return Color.parseColor(ORANGE);
+		case 2:
+			return Color.parseColor(BLUE);
+		case 3:
+			return Color.parseColor(GREEN);
+		case 4:
+			return Color.parseColor(RED);
+		case 5:
+			return Color.parseColor(YELLOW);
+		case 6:
+			return Color.parseColor(BLACK);
+
+		default:
+			return Color.parseColor(YELLOW);
+		}
+	}
+
+	public static int RouteIcon(int color) {
+		switch (color) {
+		case 1:
+			return R.drawable.icon_gesture_orange;
+		case 2:
+			return R.drawable.icon_gesture_blue;
+		case 3:
+			return R.drawable.icon_gesture_green;
+		case 4:
+			return R.drawable.icon_gesture_red;
+		case 5:
+			return R.drawable.icon_gesture_yellow;
+		case 6:
+			return R.drawable.icon_gesture_black;
+
+		default:
+			return R.drawable.icon_gesture_yellow;
+		}
 	}
 
 }

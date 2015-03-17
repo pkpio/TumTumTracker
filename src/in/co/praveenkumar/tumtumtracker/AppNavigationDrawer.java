307c27d1c190dd5862fb17f7dc714aab3c3ab74e
Praveen Kumar Pendyala <praveendath92@gmail.com>
2015-03-17 16:24:41 +0100

    Updated to latest API level and removed support libs from repo

--- src/in/co/praveenkumar/tumtumtracker/AppNavigationDrawer.java
+++ src/in/co/praveenkumar/tumtumtracker/AppNavigationDrawer.java
@@ -3,9 +3,9 @@ package in.co.praveenkumar.tumtumtracker;
 import in.co.praveenkumar.tumtumtracker.AppInterface.DrawerStateChanger;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.MenuItem;
@@ -25,7 +25,6 @@ public class AppNavigationDrawer extends ActionBarActivity implements
 		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 		mDrawerToggle = new ActionBarDrawerToggle(this, /* host Activity */
 		mDrawerLayout, /* DrawerLayout object */
-		R.drawable.ic_navigation_drawer, /* nav drawer icon to replace 'Up' caret */
 		R.string.drawer_open, /* "open drawer" description */
 		R.string.drawer_close /* "close drawer" description */
 		) {

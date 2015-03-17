7be37bf217e508a7942c6c9876bc213439cc822a
Praveen Kumar Pendyala <praveendath92@gmail.com>
2015-03-17 18:21:28 +0100

    Cleanup Browser activity

--- src/in/co/praveenkumar/tumtumtracker/BrowserActivity.java
+++ src/in/co/praveenkumar/tumtumtracker/BrowserActivity.java
@@ -3,11 +3,7 @@ package in.co.praveenkumar.tumtumtracker;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
-import android.webkit.CookieSyncManager;
-import android.webkit.JsResult;
-import android.webkit.WebChromeClient;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 public class BrowserActivity extends Activity {
 	WebView mBrowser;
@@ -31,38 +27,6 @@ public class BrowserActivity extends Activity {
 		setTitle(title);
 
 		mBrowser = (WebView) findViewById(R.id.webview);
-		CookieSyncManager.createInstance(this);
-		CookieSyncManager.getInstance().startSync();
-
-		mBrowser.getSettings().setJavaScriptEnabled(true);
-		mBrowser.getSettings().setDomStorageEnabled(true);
-		mBrowser.setWebChromeClient(new WebChromeClient() {
-			@Override
-			public boolean onJsAlert(WebView view, String url, String message,
-					JsResult result) {
-				return super.onJsAlert(view, url, message, result);
-			}
-		});
-
-		mBrowser.setWebViewClient(new WebViewClient() {
-			@Override
-			public boolean shouldOverrideUrlLoading(WebView view, String url) {
-				view.loadUrl(url);
-				// Login user if not logged in
-				// if (view.getUrl() == "") {
-
-				// }
-				return true;
-			}
-
-			@Override
-			public void onPageFinished(WebView view, String url) {
-				// view.loadUrl("javascript:"
-				// + "document.getElementById('password').value='lola';");
-				System.out.println("Page loaded!");
-			}
-		});
-
 		mBrowser.loadUrl(url);
 	}
 
